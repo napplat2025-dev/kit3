a930e8ec318ccd7ce00f49fb09ef99e1d504d596
napplat2025-dev <napplat2025@gmail.com>
2026-04-12 20:18:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https:",
+              "connect-src 'self' https://www.google-analytics.com https://formspree.io https://7aa58hgk.api.sanity.io https://cdn.sanity.io",
+              "frame-src 'self' https://www.youtube.com https://player.vimeo.com",
+              "media-src 'self' https://cdn.sanity.io",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self' https://formspree.io",
+              "upgrade-insecure-requests",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
+
 module.exports = nextConfig
